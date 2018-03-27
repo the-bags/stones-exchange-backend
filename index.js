899c40931bd823b8493cc105eafd48787464e10c
Andrii Sviatogor <av.sviatogor@gmail.com>
2018-03-27 19:43:38 +0300

    move script from inventory page to file, add "js" folder

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const app = express();
 
-// use static content in "pages" folder
+// use static content in "pages" and "js" folder
 app.use(express.static('pages'));
+app.use(express.static('js'));
 
 // listen request by server on {port}
 const port = 3000;
---
+++ js/inv-stones-add.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	const stones = ["Ruby (on Rails)", "Amethyst", "Moonstone", "Obsidian", "Pyrite"];
+	let el = document.getElementById("inventory");
+	el.textContent = stones;
+	};
