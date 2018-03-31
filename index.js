bb7effb9a3a72f6babb874ed724a9bc0026d1c5b
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-03-31 11:27:55 +0300

    STONE-17: Added mongodb and mongoose, porblem with DB

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ const port = 3000;
 app.listen(port, function () {
   console.log('server is running on localhost:' + port);
 });
+
+var mongoose = require('mongoose');
+// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+// mongoose.connect(' mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://stones-exchange-backend:stones-backend-password01@ds251737.mlab.com:51737/stones-exchange-backend');
+
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+  
+// // Connection URL
+// const url = 'mongodb://stones-exchange-backend:stones-backend-password01@ds251737.mlab.com:51737';
+//   //mongodb://<dbuser>:<dbpassword>@ds251737.mlab.com:51737/stones-exchange-backend
+// // Database Name
+// const dbName = 'stones-exchange-backend';
+  
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+  
+//   const db = client.db(dbName);
+  
+//   client.close();
+// });
