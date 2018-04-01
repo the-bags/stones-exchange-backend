20c13a7b8afdffa3aa3c7cd4f6bbd0cf3f8d2af0
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-04-01 05:51:02 +0300

    Connect DB

--- index.js
+++ index.js
@@ -13,25 +13,22 @@ app.listen(port, function () {
 });
 
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
-// mongoose.connect(' mongodb://127.0.0.1:27017/test');
-mongoose.connect('mongodb://stones-exchange-backend:stones-backend-password01@ds251737.mlab.com:51737/stones-exchange-backend');
+mongoose.connect('mongodb://backend:stonespassword01@ds115579.mlab.com:15579/stones-exchange');
+var Users = require('./models/Users');
 
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-  
-// // Connection URL
-// const url = 'mongodb://stones-exchange-backend:stones-backend-password01@ds251737.mlab.com:51737';
-//   //mongodb://<dbuser>:<dbpassword>@ds251737.mlab.com:51737/stones-exchange-backend
-// // Database Name
-// const dbName = 'stones-exchange-backend';
-  
-// // Use connect method to connect to the server
-// MongoClient.connect(url, function(err, client) {
-//   assert.equal(null, err);
-//   console.log("Connected successfully to server");
-  
-//   const db = client.db(dbName);
-  
-//   client.close();
+
+getUsers = function() {
+  return Users.find(function(err, users) {
+    if (err)
+      console.log(err);
+    console.log(users);
+  });
+};
+
+// const u1 = new Users({
+//   name: 'test',
+//   email: '1111',
+//   password: 'rrrrr'
 // });
+// u1.save();
+getUsers();
