25aba884f8ab24422fb9f64880b83a6614ab7dec
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-04-11 14:36:10 +0300

    STON-19: Added unique check for 'email' field and refactoring was done with ESLint

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-'use strict';
-const app = (require('express'))();
-const bodyParser = require('body-parser');
+"use strict";
+const app = (require("express"))();
+const bodyParser = require("body-parser");
 
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
-app.use('/', require('./routes/index'));
-app.use('/login', require('./routes/login'));
-app.use('/user', require('./routes/user'));
-app.use('/register', require('./routes/register'));
+app.use("/", require("./routes/index"));
+app.use("/login", require("./routes/login"));
+app.use("/user", require("./routes/user"));
+app.use("/register", require("./routes/register"));
 
 app.listen(port, function() {
-  console.log('\n--------------------\nServer is running\n');
-  console.log('http://localhost:' + port);
-  console.log('\nPress Ctrl+C to stop\n--------------------\n');
+    console.log("\n--------------------\nServer is running\n");
+    console.log("http://localhost:" + port);
+    console.log("\nPress Ctrl+C to stop\n--------------------\n");
 });
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://backend:stonespassword01@ds115579.mlab.com:15579/stones-exchange');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://backend:stonespassword01@ds115579.mlab.com:15579/stones-exchange");
