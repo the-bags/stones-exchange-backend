28887ca334cd818f8a9602f79a7bdebcffa8dfdf
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-04-18 14:42:44 +0300

    STONE-5: Added Socket.io

--- server.js
+++ server.js
@@ -22,3 +22,23 @@ app.listen(port, function() {
     console.log("http://localhost:" + port);
     console.log("\nPress Ctrl+C to stop\n--------------------\n");
 });
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', function(client){
+    client.emit('customEmit', {
+        massege : 'Hi, I am server'
+    });
+    client.emit('broadcast', {
+        massege: 'Hi, all'
+    });
+    client.on('say_for_server', function(data){
+        console.log('Said me client');
+        console.log(data);
+    });
+    client.on('disconnect', function(){
+        console.log('disconnect');
+    });
+    console.log('connection');
+});
+server.listen(3002);
