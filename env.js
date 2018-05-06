18619cd5207aa417b69926ed30adaab5ee2b43ba
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-05-06 13:10:07 +0300

    Use module dotenv for env file

--- env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DB_URL: 'mongodb://backend:stonespassword01@ds115579.mlab.com:15579/stones-exchange',
-    SALT_WORK_FACTOR: 5
-};
--- routes/login.js
+++ routes/login.js
@@ -2,14 +2,14 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-const DB_URL = require('../env').DB_URL;
 
 router.post("/", async (req, res) => {
   console.log('LogIn:',req.body);
   // { email: 'admin@test.com', password: 'password' }
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(process.env.DB_URL);
     const user = await User.findOne({ email: req.body.email });
     user
       ? await user.verifyPassword(req.body.password, user.password)
